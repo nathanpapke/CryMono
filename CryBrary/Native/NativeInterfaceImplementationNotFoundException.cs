42057e5f235e65353b962b167778fc29529c22b9
Sam Neirinck <sam@samneirinck.com>
2012-11-20 10:36:58 +0100

    Added base NativeMethods class

---
+++ CryBrary/Native/NativeInterfaceImplementationNotFoundException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CryEngine.Native
+{
+    [Serializable]
+    public class NativeInterfaceImplementationNotFoundException : ApplicationException
+    {
+        public Type InterfaceType { get; set; }
+
+        public NativeInterfaceImplementationNotFoundException() { }
+        public NativeInterfaceImplementationNotFoundException(Type interfaceType) { InterfaceType = interfaceType; }
+        public NativeInterfaceImplementationNotFoundException(string message) : base(message) { }
+        public NativeInterfaceImplementationNotFoundException(string message, Exception inner) : base(message, inner) { }
+        protected NativeInterfaceImplementationNotFoundException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
+    }
+}
---
+++ CryBrary/Native/NativeMethods.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace CryEngine.Native
+{
+    internal abstract class NativeMethods<T>
+    {
+        private static T _instance;
+        public static T Instance
+        {
+            get { return _instance; }
+            internal set { _instance = value; }
+        }
+
+        static NativeMethods()
+        {
+            _instance = CreateSingletonInstanceByFindingInterfaceImplementation();
+        }
+
+        protected static T CreateSingletonInstanceByFindingInterfaceImplementation()
+        {
+            var interfaceType = typeof(T);
+
+            var implementationType = GetImplementationTypeFromInterface(interfaceType);
+            if (implementationType == null)
+            {
+                throw new NativeInterfaceImplementationNotFoundException(interfaceType);
+            }
+
+            return (T)Activator.CreateInstance(implementationType, true);
+        }
+
+        private static Type GetImplementationTypeFromInterface(Type interfaceType)
+        {
+            var implementationType = Assembly.GetExecutingAssembly().GetTypes().FirstOrDefault(typeInfo => typeInfo.GetInterfaces().Any(t => t == interfaceType));
+            return implementationType;
+        }
+
+    }
+}
